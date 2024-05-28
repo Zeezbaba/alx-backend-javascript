f105f0f7d19fd3eb42e9c092b2e3cde06fdc5c88
Zeezbaba <Azeez_okeleji>
2024-05-28 14:55:36 +0000

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const output = true;
+    if (output) {
+     resolve("Success");
+    } else {
+       reject("Error");
+    }
+  });
+}
