0253dd07b9666264bd87c4d3a54bc80997350196
Zeezbaba <Azeez_okeleji>
2024-05-28 15:06:14 +0000

    Promises kept

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,4 @@
 /* Promises kept */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const output = true;
-    if (output) {
-     resolve("Success");
-    } else {
-       reject("Error");
-    }
-  });
+  return new Promise((resolve, reject) => {});
 }
