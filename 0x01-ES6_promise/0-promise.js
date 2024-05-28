b37fb522428b678ab79cfeeed9fede86df320810
Zeezbaba <Azeez_okeleji>
2024-05-28 20:29:57 +0000

    Updated Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
-/* Promises kept */
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  	return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {});
 }
