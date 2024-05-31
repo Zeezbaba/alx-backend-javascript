95c71dcecc677ad01b3bf1e1dadaa539c09d23c0
Zeezbaba <Azeez_okeleji>
2024-05-31 02:46:53 +0000

    Classes Task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  // method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
