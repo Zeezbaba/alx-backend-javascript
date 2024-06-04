ff5b9c1bae04f7a84254129a7c6d1c93521d3008
Zeezbaba <Azeez_okeleji>
2024-06-04 11:53:03 +0000

    Data manipulation task: 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,10 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+/* eslint-disable array-callback-return */
+export default function cleanSet(set, string) {
+  if (string === undefined || string.length === 0) {
     return '';
   }
-
-  let result = '';
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      if (result.length > 0) {
-        result += '-';
-      }
-      result += value.slice(startString.length);
-    }
-  }
-  return result;
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
+    .join('-');
 }
