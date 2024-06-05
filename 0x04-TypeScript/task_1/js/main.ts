ee6898e5cb11f41647a3a8bfa62889c8133e71b3
Zeezbaba <Azeez_okeleji>
2024-06-05 21:02:49 +0000

    Typescript task: 3&4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,49 @@
 interface Teacher {
-	readonly firstName: string;
-	readonly lastName: string;
-	fullTimeEmployee: boolean;
-	yearsOfExperience?: number;
-	location: string;
-	[key: string]: any;
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
 }
 
 interface Directors extends Teacher {
-	numberOfReports: number;
+  numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]. ${lastName}`;
+}
+
+interface studentClassConstructor {
+  new (firstName: string, lastName: string): studentClassInterface;
+}
+
+interface studentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements studentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+
