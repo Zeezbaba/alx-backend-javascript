4ecaf74a31ffa2527f1982ac6f8b0ee530335718
Zeezbaba <Azeez_okeleji>
2024-06-05 21:35:11 +0000

    Typescript task: 3&4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,7 +16,7 @@ interface printTeacherFunction {
 }
 
 function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName[0]. ${lastName}`;
+return `${firstName[0]}. ${lastName}`;
 }
 
 interface studentClassConstructor {
