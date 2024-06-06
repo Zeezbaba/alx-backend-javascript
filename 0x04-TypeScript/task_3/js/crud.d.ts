e6669d74255b3b5d3cb1a710a8ddc26239634def
Zeezbaba <Azeez_okeleji>
2024-06-06 15:56:04 +0000

    Typescript task: 8

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): number;
+
+export function deleteRow(rowId: RowId): void;
+
+export function updateRow(rowId: RowId, row: RowElement): number;
