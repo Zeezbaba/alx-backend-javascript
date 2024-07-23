d1c981992e04c50a8056375dc86d6cc1f2d5b997
Zeezbaba <Azeez_okeleji>
2024-07-23 14:46:39 +0000

    express server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 12345;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {});
